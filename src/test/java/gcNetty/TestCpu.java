69b8d8c628a494c29deb2bccd22d63c84f981f61
chenl0517 <1083287447@qq.com>
2018-04-25 10:19:31 +0800

    netty4.x demo

---
+++ src/test/java/gcNetty/TestCpu.java
@@ -0,0 +1,11 @@
+package gcNetty;
+
+public class TestCpu {
+	public static void main(String[] args) {
+		//System.out.println(Runtime.getRuntime().availableProcessors());
+		int aa=16;
+		String aa_str=Integer.toString(aa);
+		System.out.println(aa_str);
+	}
+
+}
